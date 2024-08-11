64484761bf7328b01f7b93e99eacef1587e1552d
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-11 20:41:01 +0530

    Simple Todo App

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,44 @@
 import { useState } from 'react';
 import Header from './components/Header'
 import { HeaderWithButton } from './components/HeaderWithButton';
+import Todo from './components/Todo';
 
 function App() {
 
-  const [title,setTitle]=useState("Parent")
-  function updateTitle(){
-    setTitle(Math.random()+"")
+  const [todos, setTodos] = useState([
+    {
+      title: "Title 1",
+      description: "Description 1"
+    },
+
+    {
+      title: "Title 2",
+      description: "Description 2"
+    },
+
+    {
+      title: "Title 3",
+      description: "Description 3"
+    },
+  ])
+
+  function AddTodo(){
+    setTodos([...todos,{
+      title:"Sample Todo",
+      description:"Sample description"
+    }])
   }
+
+
 
   return (
     <div>
-      <button onClick={updateTitle}>Update First Title</button>
-      <Header title={title} />
-      <Header title={"Title 2"} />
-      <Header title={"Title 3"} />
-      <Header title={"Title 4"} />
-      <Header title={"Title 5"} />
+
+      <button onClick={AddTodo}>Add New Todo</button>
+      {todos.map((todo)=>{
+        return <Todo title={todo.title} description={todo.description} />
+      })}
+
     </div>
   )
 }
