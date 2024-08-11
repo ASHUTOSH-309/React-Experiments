fc2aeed49f44e33bf3b56ed2ad612dbf45ae6ce3
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-12 00:45:03 +0530

    Button render Logic added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,12 @@
 
 
 
-const Todo = ({title,description}) => {
+const Todo = ({title,description,completed}) => {
 
     return <div>
         <h1>{title}</h1>
         <h2>{description}</h2>
+        <button>{completed===true? "Completed":"Mark as complete"}</button>
     </div>
 }
 
