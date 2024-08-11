b058b2907f49d78471daec8a0ac068fe63038883
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-11 17:43:22 +0530

    Button to change title

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Header from './components/Header'
+
+function App() {
+
+
+  return (
+    <div>
+      <Header title={"Title 1"} />
+      <Header title={"Title 2"} />
+    </div>
+  )
+}
+
+export default App
